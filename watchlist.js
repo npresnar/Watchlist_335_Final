25fadeadd6f760ce441522692141ab8e05c64ff9
npresnar <npresnar@terpmail.umd.edu>
2024-05-17 22:35:12 -0400

    comments

--- watchlist.js
+++ watchlist.js
@@ -7,7 +7,6 @@ const fs = require("fs");
 const dotenv = require("dotenv");
 
 
-// Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -19,21 +18,16 @@ const dbName = process.env.MONGO_DB_NAME;
 const collectionName = process.env.MONGO_COLLECTION;
 
 
-// MongoDB connection URI
-// const uri = `mongodb+srv://${username}:${password}@cluster0.9ar5xe5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${username}:${password}@cluster0.q3nnrm3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const databaseAndCollection = {
   db: process.env.MONGO_DB_NAME,
   collection: process.env.MONGO_COLLECTION,
 };
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'templates')));
 
-// Set view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'templates'));
 
@@ -69,18 +63,14 @@ app.get("/", (req, res) => {
   
   fetchWithRetry(url, options, retries=50, backoff=300)
   .then(data => {
-    // Pass the movie data to the index.ejs template
     res.render("index", { movies: data.results });
   })
   .catch(error => {
     console.error('There was a problem with your fetch operation:', error);
-    // In case of error, render the index page with an empty movie list
     res.render("index", { movies: [] });
   });
 });
 
-// Express route to render the movie page
-// Express route to render the movie page
 app.get("/movie/:id", async (req, res) => {
   const movieId = req.params.id;
   const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
@@ -101,36 +91,10 @@ app.get("/movie/:id", async (req, res) => {
   }
 });
 
-
-// POST endpoint for search
-// app.post('/search', async (req, res) => {
-//   try {
-//     // Extract the search query from the request body
-//     const { query } = req.body;
-//     const url = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(query)}`;
-//     const options = {
-//       method: 'GET',
-//       headers: {
-//         Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
-//         Accept: "application/json",
-//       },
-//     };
-
-//     const data = await fetchWithRetry(url, options, 3, 300);
-
-//     res.render('search-results', { movies: data.results });
-//   } catch (error) {
-//     console.error('Error searching for movies:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
 app.post('/search', async (req, res) => {
   try {
-      // Extract the search query from the request body
       const { query } = req.body;
 
-      // Make a fetch request to the TMDB API
       const response = await fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(query)}`, {
           method: 'GET',
           headers: {
@@ -149,7 +113,6 @@ app.post('/search', async (req, res) => {
   }
 });
 
-// GET endpoint for displaying all movies in the watchlist
 app.get('/watchlist', async (req, res) => {
   try {
       const client = new MongoClient(uri, {
@@ -177,7 +140,6 @@ app.get('/watchlist', async (req, res) => {
         return `${hours}h ${mins}m`;
       }
 
-      // Render the EJS template with the list of movies
       res.render('watchlist', { movies, formatRuntime });
   } catch (error) {
       console.error('Error fetching movies from watchlist:', error);
@@ -185,7 +147,6 @@ app.get('/watchlist', async (req, res) => {
   }
 });
 
-// POST endpoint for adding a movie to the watchlist
 app.post('/addToWatchlist', async (req, res) => {
   const { movieId, movieTitle, movieRuntime, posterPath } = req.body;
 
@@ -236,7 +197,6 @@ app.post('/removeFromWatchlist', async (req, res) => {
   main().catch(console.error);
 })
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
@@ -244,12 +204,12 @@ app.listen(PORT, () => {
 function processInput() {
   let dataInput = process.stdin.read();
   if (dataInput !== null) {
-    dataInput = dataInput.toString().trim();  // Convert buffer to string and trim whitespace
-    console.log(`Received command: ${dataInput}`);  // Echo the command back to the user
+    dataInput = dataInput.toString().trim();
+    console.log(`Received command: ${dataInput}`); 
 
     if (dataInput === "stop") {
       console.log("Shutting down the server");
-      process.exit(0);  // It's usually better to exit with code 0 for normal shutdown
+      process.exit(0);  
     } else {
       console.log(`Invalid command: ${dataInput}`);
       console.log("Type 'stop' to shutdown the server.");
